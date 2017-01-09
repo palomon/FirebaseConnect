5a14950d85a084e74ec166ee5682a76fa3b8b7a5
Wayamon <m.wayama@kkumail.com>
2017-01-09 07:29:04 +0700

    Update Profile Completed

--- app/src/main/java/kku03/chai/waya/firebaseconnect/ProfileActivity.java
+++ app/src/main/java/kku03/chai/waya/firebaseconnect/ProfileActivity.java
@@ -5,16 +5,23 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class ProfileActivity extends AppCompatActivity implements View.OnClickListener {
 
     private FirebaseAuth firebaseAuth;
     private TextView textView;
     private Button saveButton,logoutButton;
+
+    private DatabaseReference databaseReference;
+    private EditText nameEditText,phoneEditText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,6 +34,11 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
             finish();
             startActivity(new Intent(this,LoginActivity.class));
         }
+
+        databaseReference = FirebaseDatabase.getInstance().getReference();
+
+        nameEditText = (EditText)findViewById(R.id.editTextName);
+        phoneEditText =  (EditText)findViewById(R.id.editTextPhone);
 
         FirebaseUser user = firebaseAuth.getCurrentUser();
 
@@ -41,12 +53,25 @@ public class ProfileActivity extends AppCompatActivity implements View.OnClickLi
 
     }
 
+    private void saveUserInfo(){
+        String name = nameEditText.getText().toString().trim();
+        String phone = phoneEditText.getText().toString().trim();
+
+        UserInfo userInfo = new UserInfo(name,phone);
+
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+        databaseReference.child(user.getUid()).setValue(userInfo);
+
+        Toast.makeText(this,"Saved..",Toast.LENGTH_SHORT).show();
+
+    }
+
     @Override
     public void onClick(View v) {
         if(v == saveButton){
-
+            saveUserInfo();
         }
-        else if(v == logoutButton){
+        if(v == logoutButton){
             firebaseAuth.signOut();
             finish();
             startActivity(new Intent(this , LoginActivity.class));
---
+++ app/src/main/java/kku03/chai/waya/firebaseconnect/UserInfo.java
@@ -0,0 +1,19 @@
+package kku03.chai.waya.firebaseconnect;
+
+/**
+ * Created by TheButterfly on 09-Jan-17.
+ */
+
+public class UserInfo {
+
+    public String name;
+    public String phone;
+
+    public UserInfo() {
+    }
+
+    public UserInfo(String name, String phone) {
+        this.name = name;
+        this.phone = phone;
+    }
+}
